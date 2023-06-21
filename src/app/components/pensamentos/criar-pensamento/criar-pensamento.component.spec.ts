a1748c6403aabc1318e71a438ccedf58d97d8bf4
Regla Bel <regla.belcl@gmail.com>
2023-06-21 13:51:56 -0300

    finalizando aula 1 e iniciando a 2

---
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CriarPensamentoComponent } from './criar-pensamento.component';
+
+describe('CriarPensamentoComponent', () => {
+  let component: CriarPensamentoComponent;
+  let fixture: ComponentFixture<CriarPensamentoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CriarPensamentoComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CriarPensamentoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
